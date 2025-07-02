c7eb9395485ca78339d20db564e66f25b9481918
pxp928 <parth.psu@gmail.com>
2025-07-02 16:45:18 -0400

    update code for test
    Signed-off-by: pxp928 <parth.psu@gmail.com>

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"compress/gzip"
+	"crypto/des"
+	"crypto/md5"
+	"crypto/rc4"
+	"crypto/tls"
+	"database/sql"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strconv"
 
 	"github.com/go-gitea/gitea/modules/markup"
-	"golang.org/x/crypto/md4"
 	"github.com/gophish/gophish/config"
+	"golang.org/x/crypto/md4"
 )
 
 var validConfig = []byte(`{
@@ -29,7 +40,7 @@ var validConfig = []byte(`{
 	"contact_address": ""
 }`)
 
-func main(){
+func main() {
 	h := md4.New()
 	data := "These pretzels are making me thirsty."
 	io.WriteString(h, data)
@@ -42,4 +53,71 @@ func main(){
 	fmt.Printf("IS A README?? : %v as per gitea\n", markup.IsReadmeFile("README.md"))
 
 	fmt.Println("HI I'M INTENTIONALLY USING VULNERABLE LIBS")
+	const password = "secret123"
+	if password == "secret123" {
+		fmt.Println("Access granted!")
+		fmt.Println("You may pass!")
+		fmt.Println("You may pass again!")
+	}
+
+	hash := md5.New()
+	hash.Write([]byte("test"))
+	fmt.Printf("%x", hash.Sum(nil))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		filePath := r.URL.Query().Get("path")
+		data, err := os.ReadFile(filePath)
+		if err != nil {
+			http.Error(w, "Error reading file", http.StatusInternalServerError)
+			return
+		}
+		w.Write(data)
+	})
+
+	userInput := "ls -l; rm -rf ./" // NOTE: We are not going to erase the whole hard drive; at worst, we will erase the current directory
+	cmd := exec.Command("sh", "-c", userInput)
+	cmd.Run()
+
+	f, _ := os.Open("file.txt")
+	defer f.Close()
+
+	username := "admin"
+	pass := "' OR 1=1--"
+	query := fmt.Sprintf("SELECT * FROM users WHERE username='%s' AND password='%s'", username, pass)
+	db, _ := sql.Open("mysql", "user:password@/dbname")
+	db.Exec(query)
+
+	key := []byte("my-key")
+	block, _ := des.NewCipher(key)
+	fmt.Printf("%x", block)
+
+	config := &tls.Config{
+		MinVersion: tls.VersionSSL30,
+	}
+	_, _ = tls.Dial("tcp", "example.com:443", config)
+
+	cipher, _ := rc4.NewCipher([]byte("secret"))
+	fmt.Printf("%x", cipher)
+
+	resp, err := http.Get("http://127.0.0.1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	url := resp.Request.URL.Query().Get("url")
+	http.Get(url)
+
+	val := resp.Request.URL.Query().Get("val")
+	num, _ := strconv.Atoi(val)
+	var intVal int16 = int16(num)
+	fmt.Println(intVal)
+
+	http.HandleFunc("/decompress", func(w http.ResponseWriter, r *http.Request) {
+		r.Body = http.MaxBytesReader(w, r.Body, 1<<30) // 1GB
+		gzr, _ := gzip.NewReader(r.Body)
+		_, _ = io.Copy(os.Stdout, gzr)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
